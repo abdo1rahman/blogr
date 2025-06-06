93d93a65e8899fe4463c3f1c11ca4715192582bf
abdo1rahman <abdoasem2055@gmail.com>
2025-06-06 17:39:36 +0300

    still having bugs with the NavLinks.jsx

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,9 +1,8 @@
-// This will deal with the links of the navbar on mobile sizes
 import { useRef, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function NavLinks({ activeMenu, setActiveMenu }) {
   const dropdownRef = useRef(null);
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -12,7 +11,8 @@ export default function NavLinks({ activeMenu, setActiveMenu }) {
     };
     document.addEventListener("click", handleClickOutside);
     return () => document.removeEventListener("click", handleClickOutside);
-  }, []);
+  }, [setActiveMenu]);
+
   return (
     <div className="mobile-nav overlay">
       <div className="mobile-links" ref={dropdownRef}>
@@ -25,20 +25,17 @@ export default function NavLinks({ activeMenu, setActiveMenu }) {
         >
           <span className="mobile__item">Product</span>
           <img src="/images/icon-arrow-dark.svg" className="drk-arw" />
-          {activeMenu === "product" && (
-            <motion.div
-              className="dropdown__menu"
-              animate={{ x: 100 }}
-              transition={{ type: "spring", stiffness: 100 }}
-            >
-              <p className="menu__item">Overview</p>
-              <p className="menu__item">Pricing</p>
-              <p className="menu__item">MarketPlace</p>
-              <p className="menu__item">Features</p>
-              <p className="menu__item">Integrations</p>
-            </motion.div>
-          )}
         </a>
+        {activeMenu === "product" && (
+          <div className="dropdown__menu">
+            <p className="menu__item">Overview</p>
+            <p className="menu__item">Pricing</p>
+            <p className="menu__item">Marketplace</p>
+            <p className="menu__item">Features</p>
+            <p className="menu__item">Integrations</p>
+          </div>
+        )}
+
         <a
           href="#"
           className="mobile__menu"
@@ -48,19 +45,16 @@ export default function NavLinks({ activeMenu, setActiveMenu }) {
         >
           <span className="mobile__item">Company</span>
           <img src="/images/icon-arrow-dark.svg" className="drk-arw" />
-          {activeMenu === "company" && (
-            <motion.div
-              className="dropdown__menu"
-              animate={{ x: 100 }}
-              transition={{ type: "spring", stiffness: 100 }}
-            >
-              <p className="menu__item">About</p>
-              <p className="menu__item">Team</p>
-              <p className="menu__item">Blog</p>
-              <p className="menu__item">Career</p>
-            </motion.div>
-          )}
         </a>
+        {activeMenu === "company" && (
+          <div className="dropdown__menu">
+            <p className="menu__item">About</p>
+            <p className="menu__item">Team</p>
+            <p className="menu__item">Blog</p>
+            <p className="menu__item">Career</p>
+          </div>
+        )}
+
         <a
           href="#"
           className="mobile__menu"
@@ -70,18 +64,14 @@ export default function NavLinks({ activeMenu, setActiveMenu }) {
         >
           <span className="mobile__item">Connect</span>
           <img src="/images/icon-arrow-dark.svg" className="drk-arw" />
-          {activeMenu === "connect" && (
-            <motion.div
-              className="dropdown__menu"
-              animate={{ x: 100 }}
-              transition={{ type: "spring", stiffness: 100 }}
-            >
-              <p className="menu__item">Contact</p>
-              <p className="menu__item">Newsletter</p>
-              <p className="menu__item">LinkedIn</p>
-            </motion.div>
-          )}
         </a>
+        {activeMenu === "connect" && (
+          <div className="dropdown__menu">
+            <p className="menu__item">Contact</p>
+            <p className="menu__item">Newsletter</p>
+            <p className="menu__item">LinkedIn</p>
+          </div>
+        )}
       </div>
     </div>
   );
